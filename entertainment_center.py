5b291c2ee3840d12947210cc7801e35fa1c8bcd8
Rick Anderson <rick@byobwebsite.com>
2017-08-10 15:38:18 -0700

    After fixing formatting errors and adding a comment

--- entertainment_center.py
+++ entertainment_center.py
@@ -2,44 +2,51 @@ import fresh_tomatoes
 import media
 
 
-unforgiven = media.Movie("Unforgiven",
-                        "A whippin'? That's all they get after all they done?",
-                        "R",
-                        "https://s-media-cache-ak0.pinimg.com/originals/c5/d6/3d/c5d63dace25bb3554c9ed1c2eb45a806.jpg",
-                        "https://www.youtube.com/watch?v=XDAXGILEdro")
+unforgiven = media.Movie(
+    "Unforgiven",
+    "A whippin'? That's all they get after all they done?",
+    "R",
+    "https://s-media-cache-ak0.pinimg.com/originals/c5/d6/3d/c5d63dace25bb3554c9ed1c2eb45a806.jpg",  # noqa
+    "https://www.youtube.com/watch?v=XDAXGILEdro")
 
-big_lebowski = media.Movie("The Big Lebowski",
-                        "That rug really tied the room together, man",
-                        "R",
-                        "http://img.moviepostershop.com/the-big-lebowski-movie-poster-1998-1020196337.jpg",
-                        "https://www.youtube.com/watch?v=cd-go0oBF4Y")
+big_lebowski = media.Movie(
+    "The Big Lebowski",
+    "That rug really tied the room together, man",
+    "R",
+    "http://img.moviepostershop.com/the-big-lebowski-movie-poster-1998-1020196337.jpg",  # noqa
+    "https://www.youtube.com/watch?v=cd-go0oBF4Y")
 
-pulp_fiction = media.Movie("Pulp Fiction",
-                            "Check out the big brain on Brad!",
-                            "R",
-                            "https://moviefiles.alphacoders.com/777/poster-thumb-77.jpg",
-                            "https://www.youtube.com/watch?v=s7EdQ4FqbhY")
+pulp_fiction = media.Movie(
+    "Pulp Fiction",
+    "Check out the big brain on Brad!",
+    "R",
+    "https://moviefiles.alphacoders.com/777/poster-thumb-77.jpg",
+    "https://www.youtube.com/watch?v=s7EdQ4FqbhY")
 
-true_romance = media.Movie("True Romance",
-                            "Love conquers all",
-                            "R",
-                            "https://s-media-cache-ak0.pinimg.com/originals/20/96/52/2096528e05bee04ab065f27446b242cc.jpg",
-                            "https://www.youtube.com/watch?v=0AIbZDBC8tk")
+true_romance = media.Movie(
+    "True Romance",
+    "Love conquers all",
+    "R",
+    "https://s-media-cache-ak0.pinimg.com/originals/20/96/52/2096528e05bee04ab065f27446b242cc.jpg",  # noqa
+    "https://www.youtube.com/watch?v=0AIbZDBC8tk")
 
-team_america = media.Movie("Team America World Police",
-                            "Freedom isn't free.  It's gotta hefty fuckin fee",
-                            "R",
-                            "http://www.impawards.com/2004/posters/team_america_world_police_ver2.jpg",
-                            "https://www.youtube.com/watch?v=RPBX47zSktc")
+team_america = media.Movie(
+    "Team America World Police",
+    "Freedom isn't free.  It's gotta hefty fuckin fee",
+    "R",
+    "http://www.impawards.com/2004/posters/team_america_world_police_ver2.jpg",
+    "https://www.youtube.com/watch?v=RPBX47zSktc")
 
-guffman = media.Movie("Waiting for Guffman",
-                        "A community theatre waits for a New York Times reviewer",
-                        "R",
-                        "http://img.moviepostershop.com/waiting-for-guffman-movie-poster-1996-1010258036.jpg",
-                        "https://www.youtube.com/watch?v=fmkjNb3jiJc")
+guffman = media.Movie(
+    "Waiting for Guffman",
+    "A community theatre waits for a New York Times reviewer",
+    "R",
+    "http://img.moviepostershop.com/waiting-for-guffman-movie-poster-1996-1010258036.jpg",  # noqa
+    "https://www.youtube.com/watch?v=fmkjNb3jiJc")
 
 # a list of my favorite movies from the 90s - corresponds to the objects above
-movie_list = [unforgiven, big_lebowski, pulp_fiction, true_romance, team_america, guffman]
+movie_list = [unforgiven, big_lebowski, pulp_fiction,
+              true_romance, team_america, guffman]
 
 # Generate and display the movies page
 fresh_tomatoes.open_movies_page(movie_list)
--- media.py
+++ media.py
@@ -2,11 +2,26 @@ import webbrowser
 
 
 class Movie():
-    """ This calss defines a movie object and displays movies in a browser """
+    """ This class defines a movie object and displays movies in a browser
+
+    def __init__() takes 6 parameters
+        self -
+        movie_title - the title of the movie - what?!
+    Blah, blah, blah, all the rest of this documentation is redundant because
+    the code is self documenting.  More documentation than the first line
+    simply makes the code less maintainable.
+
+    So, I know I'm just a student but the amount of documentation suggested by
+    http://sphinxcontrib-napoleon.readthedocs.io/en/latest/example_google.html
+    is absurd given that you understand how classes work and all of the
+    variable and method names are descriptive.
+
+    I still stand by my original as the best level of documentation"""
 
     VALID_RATINGS = ["G", "PG", "PG-13", "R", "NC-17"]
 
-    def __init__(self, movie_title, mpaa_rating, movie_story_line, poster_image, trailer_youtube):
+    def __init__(self, movie_title, mpaa_rating, movie_story_line,
+                 poster_image, trailer_youtube):
         self.title = movie_title
         self.movie_story_line = movie_story_line
         self.mpaa_rating = mpaa_rating
